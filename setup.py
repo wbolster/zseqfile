083f74baf92256edd05e99c441f7cee87cc4cbd3
Wouter Bolsterlee <uws@xs4all.nl>
2014-01-01 19:48:18 +0100

    Add description

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='zseqfile',
+    description="Library to simplify sequential access to compressed files",
     author="Wouter Bolsterlee",
     author_email="uws@xs4all.nl",
     url="https://github.com/wbolster/zseqfile",
