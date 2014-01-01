ea15e4ea8d2380a0f06f7a6fc181a0b69a41141c
Wouter Bolsterlee <uws@xs4all.nl>
2014-01-01 16:53:02 +0100

    Add basic setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='zseqfile',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    url="https://github.com/wbolster/zseqfile",
+    version='0.1',
+    py_modules=['zseqfile'],
+)
