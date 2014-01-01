4b19d2f200c5bd57577d9328398b9ca343cd5f99
Wouter Bolsterlee <uws@xs4all.nl>
2014-01-01 15:36:53 +0100

    Implement which() to figure out executable paths

--- zseqfile.py
+++ zseqfile.py
@@ -6,11 +6,37 @@ import bz2
 import gzip
 import io
 import lzma
+import os
 
 
 #
-# Opener functions
+# Helper functions
 #
+
+def is_executable_file(path):
+    """Check whether the specified path is an executable file."""
+    return os.path.isfile(path) and os.access(path, os.X_OK)
+
+
+def which(executable):
+    """Return the path to the specified executable, or None if not found."""
+
+    # Absolute paths
+    if os.path.isabs(executable):
+        if is_executable_file(executable):
+            return executable
+        else:
+            return None
+
+    # Search $PATH
+    for dir in os.environ.get('PATH', '').split(os.pathsep):
+        file = os.path.join(dir, executable)
+        if is_executable_file(file):
+            return file
+
+    # Not found
+    return None
+
 
 def open_regular(file, mode, encoding, errors, newline):
     return io.open(file, mode=mode, encoding=encoding, errors=errors,
