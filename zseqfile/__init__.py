d10656527cf3a0fe3d47827d8d2f27fda4cb2a5c
Wouter Bolsterlee <uws@xs4all.nl>
2014-02-22 20:54:22 +0100

    Define a few convenience aliases in the public API

--- zseqfile/__init__.py
+++ zseqfile/__init__.py
@@ -2,10 +2,13 @@
 zseqfile - transparently handle compressed files
 """
 
-# Expose the public API.
 from .zseqfile import (  # noqa
     open,
     open_gzip,
     open_bzip2,
     open_lzma,
 )
+
+open_gz = open_gzip
+open_bz2 = open_bzip2
+open_xz = open_lzma
